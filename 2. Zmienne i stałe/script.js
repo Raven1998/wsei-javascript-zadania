e843138af582e5a2ce247ae0c1f5fd06ec819689
Raven1998 <72340671+Raven1998@users.noreply.github.com>
2020-10-04 14:21:27 +0200

    ZESTAW 2 - rozwiazania

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,28 @@
+//ZADANIE 1
+
+let number = 5;
+let string ="AA";
+let mix = 2+"dwa";
+let bool =true;
+let special=null;
+
+
+console.log(number);
+console.log(string);
+console.log(mix);
+console.log(bool);
+console.log(special);
+
+//ZADANIE 2
+
+let a =5;
+let b=6;
+let sum =0;
+
+console.log(a+b);
+
+//ZADANIE 3
+
+let a;
+console.log(a);
+// wynikiem jest wartosc undefined poniewaz nie zdefiniowalismy zadnej wartosci
