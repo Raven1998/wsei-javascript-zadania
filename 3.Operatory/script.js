c50db7bb0c3358fff498efb8b11514c8b1310792
Raven1998 <72340671+Raven1998@users.noreply.github.com>
2020-10-04 14:33:25 +0200

    ZESTAW 3 - Rozwiazania

---
+++ 3.Operatory/script.js
@@ -0,0 +1,56 @@
+//ZADANIE 1
+let a =true;
+let b=false;
+
+console.log(a==b);
+
+
+//ZADANIE 2
+let a =5;
+let b =3;
+let moduloResult;
+
+moduloResult = a%b;
+
+console.log(moduloResult);
+
+//ZADANIE 3
+let a ="AA";
+let b ="BB";
+let stringResult;
+
+stringResult =a+b;
+
+console.log(stringResult);
+
+//ZADANIE 4
+var someNumber =425;
+var someString ="425";
+
+console.log(someNumber==someString);
+console.log(someNumber===someString);
+
+//Pierwsze porownanie zwroci wartosc true poniewaz porowna same wartosci, drugie zwroci false poniewaz porowna rowniez typy ktore tutaj sa rozne.
+
+
+
+//ZADANIE 5
+var counter =30;
+
+counter++;
+
+console.log(counter);
+
+counter--;
+console.log(counter);
+
+
+//ZADANIE 6
+
+var a =5;
+var b=3;
+var result=null;
+
+result = a>b;
+
+console.log(result);
