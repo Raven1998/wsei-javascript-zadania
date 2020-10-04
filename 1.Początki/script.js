4ef261e55c9ea4aa5a0ea0ea0eb783c06ab79b0c
Raven1998 <72340671+Raven1998@users.noreply.github.com>
2020-10-04 14:07:25 +0200

    Zestaw 1 - rozwiazania

---
+++ 1.Początki/script.js
@@ -0,0 +1,20 @@
+//Zadanie 1
+console.log("Rafał Kruczkowski");
+
+//Zadanie 2
+console.log(12 + 32);
+console.log(52/16);
+console.log(5*2);
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+           
+//Pierwsza komenda wyswietla sie poprawnie  natomiast druga i trzecia wyswietla bledy poniewaz brakuje cudzyslowia i szuka Hello World jako zmiennej natomiast w trzecim brakuje zakonczenia cudzyslowia  
+
+
+
+
+        
